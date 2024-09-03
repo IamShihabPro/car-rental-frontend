809a88f78a6df7cb3ef5dda0669bbaa1f90ce0f9
IamShihabPro <mshihab.pro@gmail.com>
2024-09-03 18:50:51 +0600

    read me file

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { RootState } from '../store';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'https://car-rental-reservation-production.up.railway.app',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).user.token;
       if (token) {
